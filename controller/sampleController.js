3bd312b18b8803d2eb3f53bc084f539f6cace926
Anand-Jangid <jangidme88@gmail.com>
2023-09-18 15:25:51 +0530

    added sample controller file

---
+++ controller/sampleController.js
@@ -0,0 +1,8 @@
+// const {StatusCodes} = require('http-status-codes');
+// const CustomError = require('../errors');
+
+// const sampleMethod = async (req, res) =>{
+//     res.status(StatusCodes.OK).send("Hello World");
+// }
+
+// module.exports = {sampleMethod}
